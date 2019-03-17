0aba028ef6f0e14c22014ce9a5c4a9ee6b1d0b1a
Ninbous <Nibous@mail.ru>
2019-03-17 22:40:18 +0300

    added_Asteroids

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -15,6 +15,21 @@ public class PlayerController : MonoBehaviour
     public Boundary boundary;
 
     public Joystick joystick;
+    
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+    
+    void Update ()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
 
     void FixedUpdate ()
     {
